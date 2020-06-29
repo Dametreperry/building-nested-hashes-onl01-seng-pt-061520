634c165b741df5b19a7506714461c3055048f125
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-06-29 03:05:11 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,7 @@
 def second_challenge
   epic_tragedy = {
-    :montague => { :patriarch => {}, :matriarch => {}, :hero => {}, :hero_friends => {} }, 
-    :capulet => {:patriarch => {}, :matriarch => {}, :heroine => {}, :heroine_friends => {} }}
+    :montague => { :patriarch => {:name, :age}, :matriarch => {:name, :age}, :hero => {:name, :age, :status}, :hero_friends => {:name, :age} }, 
+    :capulet => {:patriarch => {:name, :age}, :matriarch => {:name, :age}, :heroine => {:name, :age, :status}, :heroine_friends => {:name, :age} }}
 
   
 end
